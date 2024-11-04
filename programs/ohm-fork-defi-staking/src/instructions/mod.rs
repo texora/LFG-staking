ce9be7c5a39752d07560d7008738ebf1c77d1be1
root <ultantaylor@gmail.com>
2024-11-04 19:07:19 +0900

    add reward token staking

--- programs/ohm-fork-defi-staking/src/instructions/mod.rs
+++ programs/ohm-fork-defi-staking/src/instructions/mod.rs
@@ -1,5 +1,7 @@
 pub mod ohm_staking;
+pub mod reward_token_staking;
 pub mod meme_coin_staking;
 
 pub use ohm_staking::*;
+pub use reward_token_staking::*;
 pub use meme_coin_staking::*;
---
+++ programs/ohm-fork-defi-staking/src/instructions/reward_token_staking/mod.rs
@@ -0,0 +1,3 @@
+pub mod reward_token_staking_initialize;
+
+pub use reward_token_staking_initialize::*;
