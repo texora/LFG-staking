d24db0fa701bc96fe036543a6e29f5abe8aca518
root <ultantaylor@gmail.com>
2024-11-04 19:12:23 +0900

    add readme & fix name

--- programs/ohm-fork-defi-staking/src/lib.rs
+++ programs/ohm-fork-defi-staking/src/lib.rs
@@ -9,7 +9,7 @@ use instructions::*;
 declare_id!("HmXTaac3Cw3xvpfnkMC1nra8QZ1pA2W4c5PQ8HyozHKx");
 
 #[program]
-pub mod ohm_fork_defi_staking {
+pub mod lfg_staking {
     use super::*;
 
     // ohm token staking initialize
