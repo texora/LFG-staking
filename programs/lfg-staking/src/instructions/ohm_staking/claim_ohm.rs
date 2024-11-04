3a344f40b2ae50cc9f7119d86907e729681242de
root <ultantaylor@gmail.com>
2024-11-04 20:46:23 +0900

    add claim_ohm

---
+++ programs/lfg-staking/src/instructions/ohm_staking/claim_ohm.rs
@@ -0,0 +1,13 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, MintTo, Transfer};
+use anchor_spl::token_interface::{TokenAccount, TokenInterface};
+
+#[derive(Accounts)]
+pub struct ClaimOhm<'info>{
+    #[account(mut)]
+    pub user: Signer<'info>
+}
+
+pub fn claim_ohm(ctx: Context<ClaimOhm>) -> Result<()>{
+    Ok(())
+}
