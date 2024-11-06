1cc91892ec0c5d9dc2f7c2991c4874fb8c6c101a
root <ultantaylor@gmail.com>
2024-11-06 18:20:51 +0900

    feat: meme coin initialize

--- programs/lfg-staking/src/instructions/meme_coin_staking/mod.rs
+++ programs/lfg-staking/src/instructions/meme_coin_staking/mod.rs
@@ -1,2 +1,11 @@
 pub mod meme_coin_initialize;
 pub use meme_coin_initialize::*;
+
+pub mod stake_meme_coin;
+pub use stake_meme_coin::*;
+
+pub mod unstake_meme_coin;
+pub use unstake_meme_coin::*;
+
+pub mod claim_reward_for_meme_coin;
+pub use claim_reward_for_meme_coin::*;
